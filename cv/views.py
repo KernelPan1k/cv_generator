a923b395408e342627c8e3d7f91e96f68a4d8619
mm <mmick@protonmail.com>
2020-05-05 22:07:20 +0200

    add view + route

--- cv/views.py
+++ cv/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import TemplateView, View
 
-# Create your views here.
+from cv.models import CV
+
+
+class CvView(View):
+    template_name = "cv/index.html"
+
+    def get(self, request, *args, **kwargs):
+        obj = get_object_or_404(CV, pk=kwargs['pk'])
+        return render(request, self.template_name, {'obj': obj})
--- cv_generator/urls.py
+++ cv_generator/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from cv import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cv/<int:pk>/', views.CvView.as_view()),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
